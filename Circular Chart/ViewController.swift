026eb80fd5f896438db0b7e7c403d0155f99fc07
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-05-25 22:54:47 +0530

    project added

---
+++ Circular Chart/ViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    let shapeLayer = CAShapeLayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addCircularBar()
+    }
+    
+    
+    private func addCircularBar() {
+        let center = view.center
+         
+         
+        let circularPath = UIBezierPath(arcCenter: center, radius: view.frame.width / 2 - 20, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+         
+         let trackLayer = CAShapeLayer()
+         trackLayer.path = circularPath.cgPath
+         trackLayer.strokeColor = UIColor.lightGray.cgColor
+         trackLayer.fillColor = UIColor.clear.cgColor
+         trackLayer.lineWidth = 10
+         view.layer.addSublayer(trackLayer)
+         
+        
+         shapeLayer.path = circularPath.cgPath
+         shapeLayer.strokeColor = UIColor.red.cgColor
+         shapeLayer.fillColor = UIColor.clear.cgColor
+         shapeLayer.lineCap = CAShapeLayerLineCap.round
+         shapeLayer.lineWidth = 10
+         shapeLayer.strokeEnd = 1
+
+         view.layer.addSublayer(shapeLayer)
+
+         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    @objc func handleTap() {
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 0
+        basicAnimation.duration = 10
+        
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "circularWheel")
+    }
+
+
+}
+
