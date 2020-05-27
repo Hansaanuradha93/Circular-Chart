5fc10f9443572c890e2c7748a2f4afc3504f21c4
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-05-27 11:28:30 +0530

    Pulse layer added

---
+++ Circular Chart/Custom Views/Layers/PulseLayer.swift
@@ -0,0 +1,13 @@
+//
+//  PulseLayer.swift
+//  Circular Chart
+//
+//  Created by Hansa Anuradha on 5/27/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class PulseLayer: CALayer {
+
+}
