b11f8593d68dccef8bbb59325837a6ed1cf833b3
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-05-31 23:01:55 +0530

    stroke layer changed

--- Circular Chart/Custom Views/Layers/StrokeLayer.swift
+++ Circular Chart/Custom Views/Layers/StrokeLayer.swift
@@ -2,9 +2,21 @@ import UIKit
 
 class StrokeLayer: CALayer {
     
+    var center: CGPoint!
+    var radius: CGFloat!
+    var strokeColor: UIColor!
     
     override init(layer: Any) { super.init(layer: layer) }
     
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    
+    init(center: CGPoint, radius: CGFloat, strokeColor: UIColor) {
+        super.init()
+        
+        self.center = center
+        self.radius = radius
+        self.strokeColor = strokeColor
+    }
 }
