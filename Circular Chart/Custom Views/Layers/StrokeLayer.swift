9ed4a115c64b392452f6f0cb852d6cbd579bc430
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-05-31 22:55:10 +0530

    StrokerLayer added

---
+++ Circular Chart/Custom Views/Layers/StrokeLayer.swift
@@ -0,0 +1,13 @@
+//
+//  StrokeLayer.swift
+//  Circular Chart
+//
+//  Created by Hansa Anuradha on 5/31/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class StrokeLayer: CALayer {
+
+}
