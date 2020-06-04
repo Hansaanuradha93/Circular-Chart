eea5dd686a9560c0e556ec655e99f7d1cd4ecfb4
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-04 22:15:30 +0530

    Code cleared

--- Circular Chart/Scenes/Double Ring/DoubleRingViewController.swift
+++ Circular Chart/Scenes/Double Ring/DoubleRingViewController.swift
@@ -44,37 +44,6 @@ extension DoubleRingViewController {
     }
     
     
-    private func createOuterRing() -> CAShapeLayer {
-        
-        let center = view.center
-         
-        let circularPath = UIBezierPath(arcCenter: center, radius: view.frame.width / 3 - 20, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi - CGFloat.pi / 2, clockwise: true)
-         
-        outerRing.path = circularPath.cgPath
-        outerRing.strokeColor = UIColor.trackStrokeColor.cgColor
-        outerRing.fillColor = UIColor.clear.cgColor
-        outerRing.lineWidth = 15
-        
-        return outerRing
-    }
-    
-    
-    private func createInnerRing() -> CAShapeLayer {
-        
-        let center = view.center
-         
-        let circularPath = UIBezierPath(arcCenter: center, radius: view.frame.width / 3 - 20 - 15, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi - CGFloat.pi / 2, clockwise: true)
-         
-        innerRing.path = circularPath.cgPath
-        innerRing.strokeColor = UIColor.outlineStrokeColor.cgColor
-        innerRing.fillColor = UIColor.backgroundColor.cgColor
-        innerRing.lineCap = CAShapeLayerLineCap.round
-        innerRing.lineWidth = 15
-        innerRing.strokeEnd = 1
-        
-        return innerRing
-    }
-    
     private func createRing(radius: CGFloat, strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
         
         let center = view.center
