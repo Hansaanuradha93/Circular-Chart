d4376c7a12237245c7cb67eec1194a1d4e2ab5e9
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-04 22:33:33 +0530

    code refactored

--- Circular Chart/Scenes/Weel Ring/WheelViewController.swift
+++ Circular Chart/Scenes/Weel Ring/WheelViewController.swift
@@ -67,58 +67,6 @@ extension WheelViewController {
         
         return ring
     }
-
-    
-    
-    private func createOuterRing() -> CAShapeLayer {
-        
-        let center = view.center
-         
-        let circularPath = UIBezierPath(arcCenter: center, radius: view.frame.width / 3 - 20, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi - CGFloat.pi / 2, clockwise: true)
-         
-        outerRing.path = circularPath.cgPath
-        outerRing.strokeColor = UIColor.outlineStrokeColor.cgColor
-        outerRing.fillColor = UIColor.clear.cgColor
-        outerRing.lineWidth = 5
-        
-        return outerRing
-    }
-    
-    
-    private func createInnerRing() -> CAShapeLayer {
-        
-        let center = view.center
-        let gap: CGFloat = 10
-         
-        let circularPath = UIBezierPath(arcCenter: center, radius: view.frame.width / 3 - 20 - gap, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi - CGFloat.pi / 2, clockwise: true)
-         
-        innerRing.path = circularPath.cgPath
-        innerRing.strokeColor = UIColor.outlineStrokeColor.cgColor
-        innerRing.fillColor = UIColor.backgroundColor.cgColor
-        innerRing.lineCap = CAShapeLayerLineCap.round
-        innerRing.lineWidth = 5
-        innerRing.strokeEnd = 1
-        
-        return innerRing
-    }
-    
-    
-    private func createFillerRing() -> CAShapeLayer {
-        
-        let center = view.center
-        let gap: CGFloat = 10
-         
-        let circularPath = UIBezierPath(arcCenter: center, radius: view.frame.width / 3 - 20 - gap / 2, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi - CGFloat.pi / 2, clockwise: true)
-         
-        fillerRing.path = circularPath.cgPath
-        fillerRing.strokeColor = UIColor.outlineStrokeColor.cgColor
-        fillerRing.fillColor = UIColor.backgroundColor.cgColor
-        fillerRing.lineCap = CAShapeLayerLineCap.round
-        fillerRing.lineWidth = gap
-        fillerRing.strokeEnd = 0
-        
-        return fillerRing
-    }
     
     
     private func addStrokeEndAnimation() {
