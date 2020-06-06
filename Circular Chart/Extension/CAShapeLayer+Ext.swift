9051ea8e700ec94f77205bf6100c513cd0939591
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-06 22:27:48 +0530

    Stroke animation added to CAShapeLayer extensions

--- Circular Chart/Extension/CAShapeLayer+Ext.swift
+++ Circular Chart/Extension/CAShapeLayer+Ext.swift
@@ -1,6 +1,17 @@
 import UIKit
 
-extension UIViewController {
+extension CAShapeLayer {
     
-    
+    func strokeEndAnimation(fromValue: CGFloat, toValue: CGFloat, duration: CFTimeInterval) {
+        
+        let strokeAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        strokeAnimation.fromValue = 1
+        strokeAnimation.toValue = 0
+        strokeAnimation.duration = 15
+        
+        strokeAnimation.fillMode = CAMediaTimingFillMode.forwards
+        strokeAnimation.isRemovedOnCompletion = false
+        
+        self.add(strokeAnimation, forKey: "strokeEnd")
+    }
 }
