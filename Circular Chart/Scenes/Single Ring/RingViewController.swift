137abdb47545d6db80ba4a6538c5f3883f028ded
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-06 22:28:07 +0530

    Single ring code refactored

--- Circular Chart/Scenes/Single Ring/RingViewController.swift
+++ Circular Chart/Scenes/Single Ring/RingViewController.swift
@@ -87,16 +87,7 @@ extension RingViewController {
     
     
     private func addStrokeEndAnimation() {
-        
-        let strokeAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        strokeAnimation.fromValue = 1
-        strokeAnimation.toValue = 0
-        strokeAnimation.duration = 15
-        
-        strokeAnimation.fillMode = CAMediaTimingFillMode.forwards
-        strokeAnimation.isRemovedOnCompletion = false
-        
-        shapeRing.add(strokeAnimation, forKey: "strokeEnd")
+        shapeRing.strokeEndAnimation(fromValue: 1, toValue: 0, duration: 15)
     }
     
     
