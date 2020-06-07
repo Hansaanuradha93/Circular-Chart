d2e5af878fdbad6ace1c004b60931d06c6350578
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-07 22:15:46 +0530

    Ring extension added

--- Circular Chart/Extension/UIViewController+Ext.swift
+++ Circular Chart/Extension/UIViewController+Ext.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-extension UIViewController {}
+extension UIViewController {
+    
+    func addRing(radius: CGFloat, strokeColor: UIColor, fillColor: UIColor, lineWidth: CGFloat = 15, strokeEnd: CGFloat = 1) -> CAShapeLayer {
+        
+        let center = view.center
+        let ring = CAShapeLayer()
+         
+        let circularPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi - CGFloat.pi / 2, clockwise: true)
+         
+        ring.path = circularPath.cgPath
+        ring.strokeColor = strokeColor.cgColor
+        ring.fillColor = fillColor.cgColor
+        ring.lineCap = CAShapeLayerLineCap.round
+        ring.lineWidth = lineWidth
+        ring.strokeEnd = strokeEnd
+        
+        return ring
+    }
+}
