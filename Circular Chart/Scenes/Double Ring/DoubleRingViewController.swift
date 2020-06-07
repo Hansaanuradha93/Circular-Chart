e25980f0f1c3c0e4340e14be3b64c5b491186087
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-07 22:18:01 +0530

    DoubleRingViewController code refactored

--- Circular Chart/Scenes/Double Ring/DoubleRingViewController.swift
+++ Circular Chart/Scenes/Double Ring/DoubleRingViewController.swift
@@ -39,30 +39,12 @@ extension DoubleRingViewController {
     
     private func addRings() {
         
-        innerRing = createRing(radius: view.frame.width / 3 - 20 - 15, strokeColor: UIColor.outlineStrokeColor, fillColor: UIColor.backgroundColor)
-        outerRing = createRing(radius: view.frame.width / 3 - 20, strokeColor: UIColor.trackStrokeColor, fillColor: UIColor.clear)
+        innerRing = addRing(radius: view.frame.width / 3 - 20 - 15, strokeColor: UIColor.outlineStrokeColor, fillColor: UIColor.backgroundColor)
+        outerRing = addRing(radius: view.frame.width / 3 - 20, strokeColor: UIColor.trackStrokeColor, fillColor: UIColor.clear)
         view.layer.addSublayer(outerRing)
         view.layer.addSublayer(innerRing)
     }
-    
-    
-    private func createRing(radius: CGFloat, strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
-        
-        let center = view.center
-        let ring = CAShapeLayer()
-         
-        let circularPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi - CGFloat.pi / 2, clockwise: true)
-         
-        ring.path = circularPath.cgPath
-        ring.strokeColor = strokeColor.cgColor
-        ring.fillColor = fillColor.cgColor
-        ring.lineCap = CAShapeLayerLineCap.round
-        ring.lineWidth = 15
-        ring.strokeEnd = 1
-        
-        return ring
-    }
-    
+
     
     private func addStrokeEndAnimation() { innerRing.strokeEndAnimation(fromValue: 1, toValue: 0, duration: 15) }
 }
